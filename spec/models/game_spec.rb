7714ae931762bdf87fa33a9313732caad451ea5f
Dylan Richards <dylanerichards@gmail.com>
2016-02-15 17:46:15 -0800

    Test #coordinates

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
 describe Game do
-  it 'is cool' do
-    expect(1).to eq 2
+  let(:game) { described_class.new }
+
+  describe "#coordinates" do
+    it 'is an array of coordinates' do
+      expect(game.coordinates).to be_an(Array)
+    end
+
+    it 'contains all of the coordinates' do
+      coords = []
+
+      %w(a b c d e f g h i j).each do |letter|
+        (1..10).each do |num|
+          coords << "#{letter}#{num}"
+        end
+      end
+
+      coords.each do |coord|
+        expect(game.coordinates.include?(coord)).to eq true
+      end
+    end
+
   end
 end
